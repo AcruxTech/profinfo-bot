528a177aa1519f043d8d65a851939d3f4c7bdfc6
AcruxTech <acrux_tech@mail.ru>
2023-01-30 20:13:26 +0300

    add switcher for (dis)enable bot

--- app/handlers/common.py
+++ app/handlers/common.py
@@ -1,14 +1,27 @@
 import asyncio
 
 from aiogram import Dispatcher, types
-from aiogram.dispatcher import FSMContext
 
 from app.utils.keyboards import get_welcome_keyboard
 from app.utils.functions import get_welcome_text
-from app.shared.constants import GROUP_ID, DELETE_AFTER
+from app.shared.constants import GROUP_ID, DELETE_AFTER, ADMIN_ID
+from app.shared.variables import is_enabled
 
 
-async def welcome_group(message: types.Message, state: FSMContext):
+async def switch_state(message: types.Message):
+    global is_enabled
+    is_enabled = not is_enabled
+    await message.answer(
+        f'Бот сейчас: <b>{"включен" if is_enabled else "выключен"}</b>\n' + \
+        f'Для <b>{"выключения" if is_enabled else "включения"}</b> отправьте команду еще раз',
+        parse_mode='HTML'
+    )
+
+
+async def welcome_group(message: types.Message):
+    if not is_enabled:
+        return
+
     ans = await message.answer(
         get_welcome_text(message.from_user.first_name), 
         reply_markup=get_welcome_keyboard()
@@ -23,6 +36,11 @@ async def welcome_group(message: types.Message, state: FSMContext):
 
 
 def register_common_handlers(dp: Dispatcher):
+    dp.register_message_handler(
+        switch_state,
+        lambda message: message.from_user.id == ADMIN_ID,
+        commands=['switch_state']
+    )
     dp.register_message_handler(
         welcome_group,
         lambda message: message.chat.id == GROUP_ID,
--- app/utils/keyboards.py
+++ app/utils/keyboards.py
@@ -1,9 +1,11 @@
 from aiogram import types
 
+from app.shared.constants import CHANNEL_USERNAME
+
 
 def get_welcome_keyboard() -> types.InlineKeyboardMarkup:
     buttons = [
-        types.InlineKeyboardButton(text='Шаг 1: подписаться на канал', url='https://t.me/tbvrfvrf'),
+        types.InlineKeyboardButton(text='Шаг 1: подписаться на канал', url='https://t.me/' + CHANNEL_USERNAME[1:]),
         types.InlineKeyboardButton(text='Шаг 2: нажать эту кнопку', callback_data='check_subscription')
     ]
     keyboard = types.InlineKeyboardMarkup()
